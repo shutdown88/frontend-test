acda0c416a3c760743cc950d02b514810535cb95
Daniele Galli <daniele.galli@eidosmedia.com>
2018-02-25 10:12:06 +0100

    Parallax and animations

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import './App.css';
 import Article from './Article/Article'
 import Post from './Post/Post'
 import articles from './articles.json'
+import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 import Waypoint from 'react-waypoint';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { navContainerFixed: true };
+    this.state = { navContainerFixed: true, imageHidden: false };
   }
 
-  _handleWaypointEnter({ previousPosition, currentPosition, event }) {
-    console.log('Enter', previousPosition, currentPosition)
+  _handleWaypointEnter() {
     this.setState({ navContainerFixed: true })
   }
 
-  _handleWaypointLeave({ previousPosition, currentPosition, event }) {
-    console.log('Leave', previousPosition, currentPosition)
-    this.setState({ navContainerFixed: false })
+  _handleWaypointLeave() {
+    this.setState({ navContainerFixed: false, navContainerTop: document.documentElement.scrollTop })
+  }
+
+  // TODO forse devo considerare anche il caso in cui leave perchè schermo piccolo e ho scrollato in alto?
+  _handleContentWaypointEnter() {
+    console.log('ContentWaypointEnter')
+    this.setState({ imageHidden: false })
+  }
+
+  _handleContentWaypointLeave() {
+    console.log('ContentWaypointLeave')
+    this.setState({ imageHidden: true })
   }
 
   getNavContainerStyle() {
@@ -30,16 +40,35 @@ class App extends Component {
     } else {
       return {
         position: 'absolute',
-        top: document.documentElement.scrollTop
+        top: this.state.navContainerTop
+      }
+    }
+  }
+
+  // margin: -188px 64px 0 64px;
+  // border-radius: 12px;
+  getContentContainerStyle() {
+    if (this.state.imageHidden) {
+      return {
+        borderRadius: '0',
+        marginLeft: '0px',
+        marginRight: '0px',
+      }
+    } else {
+      return {
+        borderRadius: '12px',
+        marginLeft: '64px',
+        marginRight: '64px',
       }
     }
   }
 
   render() {
     return (
+      <ParallaxProvider>
       <div className="App">
         <Waypoint 
-          topOffset={-350}
+          topOffset={-275}
           onEnter={this._handleWaypointEnter.bind(this)}
           onLeave={this._handleWaypointLeave.bind(this)}
         />
@@ -53,15 +82,35 @@ class App extends Component {
         <div className='App-header-container'>
           <header className="App-header" />
         </div>
-        <div className="content-container">
-          <div className="content">
-            <Post post={articles[0]}/>
-          </div>
-          <div className="boxes-container">
-            {articles.map((a, index) => <Article article={({ index, ...a})} />)}
-          </div>
-        </div>
+          <Waypoint 
+            topOffset={268}
+            onEnter={this._handleContentWaypointEnter.bind(this)}
+            onLeave={this._handleContentWaypointLeave.bind(this)}
+          />
+          <Parallax
+              offsetYMax={0}
+              offsetYMin={-20}
+            >
+            <div className="content-container" style={this.getContentContainerStyle()}>
+              <div className="content-root">
+              <div className="content">
+                <Post post={articles[0]}/>
+              </div>
+              <Parallax
+                offsetYMax={150}
+                offsetYMin={-40}
+                slowerScrollRate={true}
+              >
+                <div className="boxes-container">
+                  {articles.map((a, index) => <Article article={({ index, ...a})} />)}
+                </div>
+              </Parallax>
+              </div>
+            </div>
+          </Parallax>
+          
       </div>
+      </ParallaxProvider>
     );
   }
 }
